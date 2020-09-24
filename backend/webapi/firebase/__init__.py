af57f3997c8be968f314e609a5dcdcba0c7c8410
masdika ID <masdikaid@gmail.com>
2020-09-24 19:52:02 +0700

    adding new feature to init

--- backend/webapi/firebase/__init__.py
+++ backend/webapi/firebase/__init__.py
@@ -1,2 +1,2 @@
 from .auth import userRegisterAuth, checkToken, userUpdateData, userDisable, userGet, userGetWithEmail, userGetAll, userRequestVerifyMail, userDelete, userIsVerified, userIsActive
-from .firestore import createShorterUrl, getVisitor, setVisitorData, getUrlData, getUrlDataByUser, getAllUrlData, setExpirationData
+from .firestore import createShorterUrl, getVisitor, setVisitorData, getUrlData, getUrlDataByUser, getAllUrlData, setExpirationData, updateUrlData, deleteUrlData
