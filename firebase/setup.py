0ec97b74a0b7e2bf7d138a5026e7c1e402c974eb
masdika ID <masdikaid@gmail.com>
2020-09-26 15:54:01 +0700

    config cred

--- firebase/setup.py
+++ firebase/setup.py
@@ -1,4 +1,17 @@
-from firebase_admin import initialize_app, auth, firestore, exceptions
+import os
+from firebase_admin import initialize_app, auth, firestore, exceptions, credentials
+cred = {
+  "type": os.environ["TYPE"],
+  "project_id": os.environ["PROJECT_ID"],
+  "private_key_id": os.environ["PRIVATE_KEY_ID"],
+  "private_key": os.environ["PRIVATE_KEY"],
+  "client_email": os.environ["CLIENT_EMAIL"],
+  "client_id": os.environ["CLIENT_ID"],
+  "auth_uri": os.environ["AUTH_URI"],
+  "token_uri": os.environ["TOKEN_URI"],
+  "auth_provider_x509_cert_url": os.environ["AUTH_PROVIDER_X509_CERT_URL"],
+  "client_x509_cert_url": os.environ["CLIENT_X509_CERT_URL"],
+}
 
-initialize_app()
+initialize_app(credentials.Certificate(cred))
 db = firestore.client()
