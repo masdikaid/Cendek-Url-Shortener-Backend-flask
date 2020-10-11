c27a0b4429cfe9dff88a2561127bf5d127aeecc1
masdika ID <masdikaid@gmail.com>
2020-10-11 19:50:09 +0700

    renaming index file

---
+++ app.py
@@ -0,0 +1,21 @@
+import sys, os, datetime
+sys.path.append(os.path.dirname(__file__))
+from api import ApiUserRegister, ApiUserManager, ApiAdminGetCreateUser, ApiAdminUserManager, VerificationHandler, ApiUserGetCreateUrl, ApiAnonGetCreateUrl, ApiUserUrlManager, UrlGetRedirect
+from flask import Flask, request, redirect, render_template
+from flask_restful import Api
+
+
+app = Flask(__name__)
+api = Api(app)          
+
+api.add_resource(ApiAnonGetCreateUrl, '/')
+api.add_resource(ApiUserManager, '/account/')
+api.add_resource(ApiUserRegister, '/account/signup/')
+api.add_resource(ApiUserGetCreateUrl, '/account/urls/')
+api.add_resource(ApiUserUrlManager, '/account/urls/<string:urlid>/')
+api.add_resource(VerificationHandler, '/account/verification/')
+api.add_resource(ApiAdminGetCreateUser, '/admin/users/')
+api.add_resource(ApiAdminUserManager, '/admin/users/<string:user_id>/')
+
+if __name__ == '__main__':
+    app.run()
